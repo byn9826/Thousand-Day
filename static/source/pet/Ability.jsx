cc27ce9b6ace70709c3af855a93913f14350b6fb
Yinan Bao <byn9826@gmail.com>
2017-02-04 22:36:45 -0500

    add progress

--- static/source/pet/Ability.jsx
+++ static/source/pet/Ability.jsx
@@ -1,5 +1,15 @@
 import React, {Component} from 'react';
+import Progress from '../snippet/display/Progress';
 class Ability extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            progress: 18
+        };
+    }
+    clickButton() {
+        this.setState({progress:120});
+    }
     render() {
         return(
             <section id="ability" className="right">
@@ -9,6 +19,10 @@ class Ability extends Component {
                 <h4>Cure: 100</h4>
                 <h4>Swift: 100</h4>
                 <h4>Lucky: 100</h4>
+                <div>
+                    <Progress progress={this.state.progress} max="103" width="30%" height="12px" />
+                </div>
+                <button type="button" value="submit" onClick={this.clickButton.bind(this)} />
             </section>
         );
     }
