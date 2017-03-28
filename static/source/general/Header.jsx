0f38faaa0de098a3c0c67e88566add81975f047d
Yinan Bao <byn9826@gmail.com>
2017-03-28 14:13:23 -0400

    update

--- static/source/general/Header.jsx
+++ static/source/general/Header.jsx
@@ -1,10 +1,6 @@
 import React, {Component} from "react";
 import Random from "../snippet/display/Random";
-import Glogin from "../snippet/social/Glogin";
 class Header extends Component {
-	googleLogin(user) {
-		console.log(user);
-	}
 	render () {
     	let headerStyle = {
 			position: "fixed",
@@ -28,24 +24,6 @@ class Header extends Component {
 			fontStyle: "italic",
 			verticalAlign: "middle"
 		};
-		let headerNavStyle = {
-			float: "right",
-			marginRight: "10%",
-			verticalAlign: "middle",
-			width: "30%"
-		};
-		let navLinkStyle = {
-			float: "left",
-			textAlign: "center",
-			width: "25%",
-			padding: "0"
-		};
-		let headerUserStyle = {
-			float: "right",
-			marginRight: "10%",
-			verticalAlign: "middle",
-			width: "10%"
-		};
 		let randomContent = [
 			"The site is still under development"
 		];
@@ -53,14 +31,6 @@ class Header extends Component {
 			<header style={headerStyle}>
 				<img src="/img/logo.png" alt="logo" style={headerLogoStyle} />
 				<Random content={randomContent} font="h5" style={headerSloganStyle} />
-				{/*
-				<div style={headerUserStyle}>
-					<Glogin googleLogin={this.googleLogin.bind(this)} clientId="168098850234-fsq84pk4cae97mlj0k464joc21cgqjvv.apps.googleusercontent.com" />
-				</div>
-				*/}
-				<nav style={headerNavStyle}>
-					<h4><a href="/" style={navLinkStyle}>Home</a></h4>
-				</nav>
 			</header>
 		);
 	}
