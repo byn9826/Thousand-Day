def840d7e752a0bfb809d67619eb99a98a144701
Yinan Bao <byn9826@gmail.com>
2017-05-23 14:13:47 -0400

    about me page

--- static/source/general/Footer.jsx
+++ static/source/general/Footer.jsx
@@ -6,9 +6,9 @@ class Footer extends Component {
 				<h6>© 2017-2018 Thousanday.com</h6>
 				<h6><a href="https://github.com/byn9826/Thousand-Day" target="__blank">Source Code</a></h6>
 				<h6><a href="https://github.com/byn9826/Thousand-Day/issues" target="__blank">Report</a></h6>
-				<h6><a href="https://github.com/byn9826/Thousanday-React" target="__blank">Thousanday-React</a></h6>
 				<h6><a href="http://glyphicons.com" target="__blank">Glyphicons</a></h6>
 				<h6><a href="/terms&privacy" target="__blank">Terms&Privacy Policy</a></h6>
+				<h6><a href="/about" target="__blank">About</a></h6>
 			</footer>
 		);
 	}
